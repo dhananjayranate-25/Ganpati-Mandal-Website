8ced7917a45a57ed7403fbd57b224c5f981ba651
Dhananjay Ranate <dhananjayranate25@gmail.com>
2026-06-02 23:19:07 +0530

    Fix API_URL for deployment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3003/api';
+const API_URL = '/api';
 
 let currentPage = 'home';
 let yearData = {};
--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3002/api';
+const API_URL = '/api';
 
 document.addEventListener('DOMContentLoaded', () => {
     createParticles();
